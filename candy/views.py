8a5e76b9f5803daf41a481c6a034fa88e24a1d00
Dileepkumar369 <dileepkumarabn655@gmail.com>
2021-04-05 18:42:05 +0530

    first commit

---
+++ candy/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def benz(request):
+    return HttpResponse("<h3>WELCOME TO CAR'S WORLD</h3>")
+
+# Create your views here.
